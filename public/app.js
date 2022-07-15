758a85b04c78e81db03528fd1f78805a9d68c964
AnTo3777 <an.moutier123@gmail.com>
2022-07-15 11:57:54 +0200

    v1

--- public/app.js
+++ public/app.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    var t0Display = document.getElementById("t0")
+    var t1Display = document.getElementById("t1")
+    var t2Display = document.getElementById("t2")
+    var t3Display = document.getElementById("t3")
+    var t4Display = document.getElementById("t4")
+    var t5Display = document.getElementById("t5")
+    var t6Display = document.getElementById("t6")
+
+
+
     const socket = io()
+
+
+
+    // myConnection
+    socket.on('myConnection', () => {
+        console.log('hello')
+              
+    })
+
 
 
 
@@ -18,18 +38,27 @@ document.addEventListener('DOMContentLoaded', () => {
       
         // Entourer en gras le joueur
         document.getElementById('p' + String(playerNum)).style.border = "3px solid black"
+       document.getElementById('p' + String(playerNum)).style.zIndex = "1"
+
 
         // Placement initial
         xPosition = 50 + 300*Math.random()
         yPosition = 50 + 300*Math.random()
 
+
         socket.on('refresh-positions', data => {
             for(let i = 0; i<3; i++){
                 if(i != playerNum){
-                    document.getElementById('p' + String(i)).style.left = data[i] + 'px'
-                    document.getElementById('p' + String(i)).style.top = data[i+3] + 'px'
+                    document.getElementById('p' + String(i)).style.left = data[i+1] + 'px'
+                    document.getElementById('p' + String(i)).style.top = data[i+4] + 'px'
+                    document.getElementById('p' + String(i)).style.zIndex = data[0][0][i]
                 }
-                
+                // Log
+                for(let i=0; i<7; i++){
+                    document.getElementById("t" + String(i)).innerHTML = data[7][0][i]
+                }
+
+
             }
         })
 
@@ -71,31 +100,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (Keys.up) {
                 if(yPosition > 0) {
-                    player.style.top = (yPosition - 4) + "px";
+                    player.style.top = (yPosition - 3) + "px";
                 }
                     
             }
             else if (Keys.down) {
                 if(yPosition < 450)
-                player.style.top = (yPosition + 4) + "px";
+                player.style.top = (yPosition + 3) + "px";
             }
         
             if (Keys.left) {
                 if(xPosition > 0)
-                player.style.left = (xPosition - 4) + "px"
+                player.style.left = (xPosition - 3) + "px"
             }
             else if (Keys.right) {
                 if(xPosition < 450)
-                player.style.left = (xPosition + 4) + "px"
+                player.style.left = (xPosition + 3) + "px"
             }
 
 
             dataPosition = [xPosition, yPosition, playerNum]
             socket.emit('position-player', dataPosition)
 
-        },20);
-
-
+        },10);
 
 
 
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const socketio = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-let pos_x_0 = -60
-let pos_x_1 = -60
-let pos_x_2 = -60
-let pos_y_0 = -5
-let pos_y_1 = -50
-let pos_y_2 = -105
+let pos_x_0 = 0
+let pos_x_1 = 60
+let pos_x_2 = 120
+let pos_y_0 = 10
+let pos_y_1 = 10
+let pos_y_2 = 10
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")))
@@ -22,6 +22,10 @@ server.listen(PORT)
 
 // Handle a socket connection request from web client
 const connections = [null, null, null]
+const data_zI = ["-1", "-1", "-1"]
+
+var nb_log = 0
+var data_text = ["", "", "", "", "", "", ""]
 
 
 io.on('connection', socket => {
@@ -29,6 +33,7 @@ io.on('connection', socket => {
 
     // Find an available player number
     let playerIndex = -1;
+
     for (const i in connections) {
         if (connections[i] === null) {
         playerIndex = i
@@ -36,10 +41,84 @@ io.on('connection', socket => {
         }
     }
     connections[playerIndex] = false
+    data_zI[playerIndex] = "1"
+
+
+    // Log
+    nb_log ++
+
+    console.log(nb_log)
+    if(nb_log > 7){
+
+        data_text[0] = data_text[1]
+        data_text[1] = data_text[2]
+        data_text[2] = data_text[3]
+        data_text[3] = data_text[4]
+        data_text[4] = data_text[5]
+        data_text[5] = data_text[6]
+        
+        data_text[6] = `Player ${playerIndex} has connected`
+
+    }
+    else {
+        deja_modife = false
+        for(let i=0; i<=7; i++) {
+            if(data_text[i] == "" && deja_modife == false){
+                data_text[i] = `Player ${playerIndex} has connected`
+                 deja_modife = true
+
+                //data_text[i] = `Player ${data_log[0]} ${data_log[1]}`
+                //deja_modife = true
+            }
+        }
+    }
+
+    
 
     // Tell the connecting client what player number they are
     socket.emit('player-number', playerIndex)
     console.log(`Player ${playerIndex} has connected`)
+
+
+    // Handle Diconnect
+    socket.on('disconnect', () => {
+        console.log(`Player ${playerIndex} disconnected`)
+        connections[playerIndex] = null
+        data_zI[playerIndex] = -1
+        if(playerIndex == 0) {
+            pos_x_0 = 0
+            pos_y_0 = 0
+        }
+        if(playerIndex == 1) {
+            pos_x_1 = 0
+            pos_y_1 = 0
+        }
+        if(playerIndex == 2) {
+            pos_x_2 = 0
+            pos_y_2 = 0
+        }
+
+        nb_log ++
+        if(nb_log >= 7){
+        data_text[0] = data_text[1]
+        data_text[1] = data_text[2]
+        data_text[2] = data_text[3]
+        data_text[3] = data_text[4]
+        data_text[4] = data_text[5]
+        data_text[5] = data_text[6]
+        data_text[6] = `Player ${playerIndex} disconnected`
+        }
+        else {
+            deja_modife = false
+            for(let i=0; i<=7; i++) {
+                if(data_text[i] == "" && deja_modife == false){
+                    data_text[i] = `Player ${playerIndex} disconnected`
+                    deja_modife = true
+                }
+            }
+        }
+
+    })
 
 
     // maj des positions
@@ -58,18 +137,13 @@ io.on('connection', socket => {
             pos_y_2 = dataPosition[1]
         }
 
-        data = [pos_x_0, pos_x_1, pos_x_2, pos_y_0, pos_y_1, pos_y_2]
+        data = [[data_zI], pos_x_0, pos_x_1, pos_x_2, pos_y_0, pos_y_1, pos_y_2, [data_text]]
 
-        
-    
+        socket.emit('refresh-positions', data)
 
-        setInterval(function(){
-            socket.emit('refresh-positions', data)
+    })
 
-        },20);
+
 
     
-    
-    
-      })
 })
